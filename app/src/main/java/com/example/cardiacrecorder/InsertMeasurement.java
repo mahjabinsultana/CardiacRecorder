b5c87bb2b42326ef96b6158b236407cb58791e95
bhuiyanmahdi <mahdibhuiyan01@gmail.com>
2023-05-21 13:44:36 +0600

    add measurement work1

---
+++ app/src/main/java/com/example/cardiacrecorder/InsertMeasurement.java
@@ -0,0 +1,49 @@
+package com.example.cardiacrecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class InsertMeasurement extends AppCompatActivity {
+
+    TextView dateView;
+    TextView timeView;
+
+    DatePickerDialog.OnDateSetListener setListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_measurement3);
+
+        dateView = findViewById(R.id.dateId);
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        dateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(InsertMeasurement.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,setListener,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month=month+1;
+                String date = day+" - "+month+" - "+year;
+                dateView.setText(date);
+            }
+        };
+    }
+}
