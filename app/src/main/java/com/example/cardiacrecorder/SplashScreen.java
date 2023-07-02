4004b49be67f037a349c283dee09b6cb474da833
bhuiyanmahdi <mahdibhuiyan01@gmail.com>
2023-07-02 20:52:44 +0600

    login reg done

--- app/src/main/java/com/example/cardiacrecorder/SplashScreen.java
+++ app/src/main/java/com/example/cardiacrecorder/SplashScreen.java
@@ -17,7 +17,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i= new Intent(getApplicationContext(), MainActivity.class);
+                Intent i= new Intent(getApplicationContext(), SignUp.class);
                 startActivity(i);
                 finish();
             }
